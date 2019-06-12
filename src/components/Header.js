0877c779688cf895e2f24271d7fd33825b1efefb
leogoes <leonardogs.goes@gmail.com>
2019-06-12 13:51:37 -0300

    Feed and New Class

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Header.css';
+// import { Container } from './styles';
+
+export default function Header() {
+  return (
+    <div />
+  );
+}
---
+++ src/pages/Feed.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+
+class Feed extends Component
+{
+    render(){
+        return(
+            <h1>Feed</h1>
+        )
+    }
+}
+
+export default Feed
